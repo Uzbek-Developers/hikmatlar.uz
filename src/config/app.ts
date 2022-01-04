8a691d9dbd421310ec8962abc8e0230f97b2b683
akhmadiy <ahmadulinjs@gmail.com>
2022-01-04 14:24:16 +0500

    feat: add default user credentials

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,8 @@
 const appConfig = {
   environment: process.env.NODE_ENV || 'development',
-  port: process.env.APP_PORT || 8080
+  port: process.env.APP_PORT || 8080,
+  adminPassword: process.env.ADMIN_USER_PASSWORD,
+  adminUser: process.env.ADMIN_USER
 };
 
 export default appConfig;
