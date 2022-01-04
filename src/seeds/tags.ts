abfd40cae3966f82930030f84b8dbd0040f34735
akhmadiy <ahmadulinjs@gmail.com>
2022-01-04 14:35:45 +0500

    ref: move to src/seds

---
+++ src/seeds/tags.ts
@@ -0,0 +1,19 @@
+import { getRepository } from 'typeorm';
+import * as faker from 'faker';
+
+import { Tag } from '../entities/Tag';
+export default async function tagsSeed() {
+  try {
+    const tagRepo = getRepository(Tag);
+
+    for (let i = 0; i < 10; i++) {
+      const tag = tagRepo.create({
+        name: faker.unique(faker.hacker.noun, [], { maxRetries: 0, maxTime: 1 })
+      });
+      await tagRepo.save(tag);
+    }
+    console.log('Create tags successfully!');
+  } catch (e) {
+    console.log(e);
+  }
+}
