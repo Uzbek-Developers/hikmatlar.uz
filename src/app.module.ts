b002f2cfcbde04d58c650fa11fc210a9768d4435
akhmadiy <ahmadulinjs@gmail.com>
2022-01-19 13:20:13 +0500

    ref: import author module & typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RootModule } from './modules/root/root.module';
+import { AuhorModule } from './modules/author/author.module';
 
 @Module({
-  imports: [RootModule]
+  imports: [TypeOrmModule.forRoot(), RootModule, AuhorModule]
 })
 export class AppModule {}
