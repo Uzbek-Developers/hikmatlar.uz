22a8e7936d8bae6e1b38fd13e770cfe1b115fd6c
akhmadiy <ahmadulinjs@gmail.com>
2022-01-22 18:12:08 +0500

    feat: setup quote module

---
+++ src/modules/quote/quote.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { QuoteService } from './quote.service';
+import { QuoteRepository } from './quote.repository';
+@Module({
+  imports: [TypeOrmModule.forFeature([QuoteRepository])],
+  exports: [QuoteService],
+  providers: [QuoteService]
+})
+export class RootModule {}
