dc2f104be343682f000232ab37ab1b9fe8559891
akhmadiy <ahmadulinjs@gmail.com>
2022-01-23 22:42:28 +0500

    test: Add test to Authors service GroupByFirstChar

--- src/modules/author/test/author.service.spec.ts
+++ src/modules/author/test/author.service.spec.ts
@@ -6,6 +6,7 @@ import 'jest-extended';
 
 describe('AuthorService', () => {
   let service: AuthorService;
+  let repository: AuthorRepository;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
@@ -16,6 +17,7 @@ describe('AuthorService', () => {
     }).compile();
 
     service = module.get<AuthorService>(AuthorService);
+    repository = module.get<AuthorRepository>(AuthorRepository);
   });
 
   it('should be defined', () => {
@@ -27,6 +29,53 @@ describe('AuthorService', () => {
     expect(getLastAuthors).toEqual(expect.toBeArray());
     getLastAuthors.forEach((author) => {
       expect(author).toContainAnyKeys(['id', 'full_name', 'description']);
+    });
+  });
+
+  describe('Authors group by firs char', () => {
+    it('should be get authors group by first char', async () => {
+      const authors = [
+        {
+          full_name: 'Abu Bakr r.a',
+          description: ''
+        },
+        {
+          full_name: 'Umar r.a',
+          description: ''
+        },
+        {
+          full_name: 'Usmon r.a',
+          description: ''
+        },
+        {
+          full_name: 'Ali r.a',
+          description: ''
+        },
+        {
+          full_name: 'Bilal r.a',
+          description: ''
+        }
+      ];
+      const data = await repository.insert(authors);
+      const authorIds: string[] = data.raw.map((author) => author.id);
+      const authorsGroupByFirstChar = await service.GroupByFirstChar();
+      console.log(authorsGroupByFirstChar);
+      expect(authorsGroupByFirstChar).toEqual(expect.toBeArray());
+
+      authorsGroupByFirstChar.forEach((group) => {
+        expect(group).toContainAnyKeys(['char_group', 'authors']);
+        expect(group.char_group).toBeString();
+        expect(group.authors).toEqual(expect.toBeArray());
+        group.authors.forEach((author) => {
+          expect(author).toContainAnyKeys(['id', 'full_name', 'description']);
+        });
+      });
+      expect(authorsGroupByFirstChar).toIncludeAllPartialMembers([
+        { char_group: 'a' },
+        { char_group: 'b' },
+        { char_group: 'u' }
+      ]);
+      await repository.delete(authorIds);
     });
   });
 });
