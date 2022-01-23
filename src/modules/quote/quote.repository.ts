5a85ba4d4aba719b1a301157fad759d28e6a4fa0
akhmadiy <ahmadulinjs@gmail.com>
2022-01-23 21:34:19 +0500

    refactor: add quote author

--- src/modules/quote/quote.repository.ts
+++ src/modules/quote/quote.repository.ts
@@ -10,7 +10,17 @@ export class QuoteRepository extends Repository<Quote> {
   async findByRandomOrder(): Promise<Quote[]> {
     try {
       return await getConnection().query(
-        'select * from quotes order by random() limit $1;',
+        `select 
+          q.id,
+          q."content",
+          row_to_json(a.*) as author,
+          q."views",
+          q.cover_img,
+          q.created_at,
+          q.updated_at
+        from quotes q
+        left outer join authors a on q.author_id = a.id
+        order by random() limit $1;`,
         [10]
       );
     } catch (err) {
