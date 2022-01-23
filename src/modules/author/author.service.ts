4e222766befbf5fdb73d6afcd40b348fe8f8cd1c
akhmadiy <ahmadulinjs@gmail.com>
2022-01-23 22:41:33 +0500

    feat: add authors group by first char

--- src/modules/author/author.service.ts
+++ src/modules/author/author.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 import { AuthorRepository } from './auhtor.repository';
 import { Author } from '../../entities/Author';
+import { AuthorsGroupByFirstChar } from './dto/AuthorsGroupByFirstChar';
 
 @Injectable()
 export class AuthorService {
   constructor(private readonly authorRepository: AuthorRepository) {}
   getLastAuthors(): Promise<Author[]> {
     return this.authorRepository.getLastAuthors();
+  }
+  GroupByFirstChar(): Promise<AuthorsGroupByFirstChar[]> {
+    return this.authorRepository.getAuthorsGroupByFirstChar(10);
   }
 }
