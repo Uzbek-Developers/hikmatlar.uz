fef4136c555762e5c728dce8e6cd78bf33b107f2
akhmadiy <ahmadulinjs@gmail.com>
2022-01-23 20:15:57 +0500

    refactor: register modules via nest routermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
+import { RouterModule } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RootModule } from './modules/root/root.module';
 import { AuhorModule } from './modules/author/author.module';
-
+import { QuoteModule } from './modules/quote/quote.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), RootModule, AuhorModule]
+  imports: [
+    TypeOrmModule.forRoot(),
+    RouterModule.register([
+      {
+        path: '/',
+        module: RootModule
+      },
+      {
+        path: '/authors',
+        module: AuhorModule
+      },
+      {
+        path: '/quotes',
+        module: QuoteModule
+      }
+    ])
+  ]
 })
 export class AppModule {}
