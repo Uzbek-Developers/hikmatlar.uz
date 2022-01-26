b0c208c1d3bd771818f87fbbfc322a1ca2e626ec
akhmadiy <ahmadulinjs@gmail.com>
2022-01-26 19:20:16 +0500

    feat: exclude api docs

--- src/modules/root/root.controller.ts
+++ src/modules/root/root.controller.ts
@@ -1,10 +1,14 @@
 import { Get, Controller } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { Page } from '../../shared/decorators/page.decorator';
 import { RootService } from './root.service';
 
+//
 @Controller()
 export class RootController {
   constructor(private readonly rootService: RootService) {}
+
+  @ApiExcludeEndpoint()
   @Get('/')
   @Page('index')
   async root() {
--- src/modules/tags/tags.controller.ts
+++ src/modules/tags/tags.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { TagsService } from './tags.service';
 import { Page } from '../../shared/decorators/page.decorator';
 
@@ -6,6 +7,7 @@ import { Page } from '../../shared/decorators/page.decorator';
 export class TagsController {
   constructor(private readonly tagService: TagsService) {}
 
+  @ApiExcludeEndpoint()
   @Get('/')
   @Page('tags')
   async tagsList() {
