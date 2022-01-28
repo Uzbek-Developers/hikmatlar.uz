9736ed1273cc2ee3a932d0d9eb556478f2fac7dd
akhmadiy <ahmadulinjs@gmail.com>
2022-01-28 23:01:43 +0500

    feat: implement create service

--- src/modules/tags/tags.service.ts
+++ src/modules/tags/tags.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { TagsGroupByFirstChar } from './dto/TagsGroupByFirstChar';
 import { TagRepository } from './tags.repository';
-
+import { CreateTagDto } from './dto/CreateTag';
+import { Tag } from '../../entities/Tag';
 @Injectable()
 export class TagsService {
   constructor(private readonly tagsRepository: TagRepository) {}
 
   GroupByFirstChar(): Promise<TagsGroupByFirstChar[]> {
     return this.tagsRepository.getTagsGroupByFirstChar(10);
+  }
+
+  create(tag: CreateTagDto): Promise<Tag> {
+    const authorModel = this.tagsRepository.create(tag);
+    return this.tagsRepository.save(authorModel);
   }
 }
