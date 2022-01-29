1dda5019e4b9128473ecfe6552d25ed5622a5b62
Ibrat <ibrat@abutech.uz>
2022-01-29 12:10:40 +0500

    feat: delete author

--- src/modules/api/authors/author-api.controller.ts
+++ src/modules/api/authors/author-api.controller.ts
@@ -1,14 +1,24 @@
-import { Post, Controller, Body } from '@nestjs/common';
+import {
+  Post,
+  Controller,
+  Body,
+  Delete,
+  Param,
+  NotFoundException
+} from '@nestjs/common';
 import { AuthorService } from '../../author/author.service';
 import { CreateAuthorDto } from '../../author/dto/CreateAuthor';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags
 } from '@nestjs/swagger';
 import { Author } from '../../../entities/Author';
 import { AuthorDto } from '../../author/dto/AuthorDto';
 import { ValidationErrorExeption } from '../../../shared/exception/ValidationErrorExeption';
+import { DeleteResult } from 'typeorm';
 
 @Controller('/authors')
 @ApiTags('authors')
@@ -20,5 +30,28 @@ export class AuthorDashboardController {
   @ApiBadRequestResponse({ type: ValidationErrorExeption })
   async createAuthor(@Body() author: CreateAuthorDto): Promise<Author> {
     return this.authorService.create(author);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ description: 'OK' })
+  @ApiNotFoundResponse({
+    description: 'Not Found',
+    schema: {
+      type: 'String',
+      example: {
+        statusCode: 404,
+        message: 'Author not found!',
+        error: 'Not Found'
+      }
+    }
+  })
+  async deleteAuthor(@Param('id') id: string): Promise<DeleteResult> {
+    const author = await this.authorService.getAuthor(id);
+
+    if (author === undefined) {
+      throw new NotFoundException('Author not found!');
+    }
+
+    return this.authorService.delete(id);
   }
 }
--- src/modules/author/author.service.ts
+++ src/modules/author/author.service.ts
@@ -4,6 +4,7 @@ import { AuthorRepository } from './auhtor.repository';
 import { Author } from '../../entities/Author';
 import { AuthorsGroupByFirstChar } from './dto/AuthorsGroupByFirstChar';
 import { CreateAuthorDto } from './dto/CreateAuthor';
+import { DeleteResult } from 'typeorm';
 @Injectable()
 export class AuthorService {
   constructor(private readonly authorRepository: AuthorRepository) {}
@@ -17,5 +18,13 @@ export class AuthorService {
   create(author: CreateAuthorDto): Promise<Author> {
     const authorModel = this.authorRepository.create(author);
     return this.authorRepository.save(authorModel);
+  }
+
+  getAuthor(id: string): Promise<Author> {
+    return this.authorRepository.findOne(id);
+  }
+
+  delete(id: string): Promise<DeleteResult> {
+    return this.authorRepository.delete(id);
   }
 }
