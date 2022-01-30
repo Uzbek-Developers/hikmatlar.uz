271718170650a904b6e5be1c24c0846d7081a9a4
akhmadiy <ahmadulinjs@gmail.com>
2022-01-30 23:48:54 +0500

    feat: move author get by id functional to quote

--- src/modules/author/auhtor.repository.ts
+++ src/modules/author/auhtor.repository.ts
@@ -44,12 +44,4 @@ export class AuthorRepository extends Repository<Author> {
       this.logger.error(err);
     }
   }
-
-  async getQuoteById(id: string) {
-    try {
-      return this.findOne({ id }, { relations: ['author', 'tags'] });
-    } catch (err) {
-      this.logger.error(err);
-    }
-  }
 }
