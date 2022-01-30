ddc463a1e5c13b2202d10a5874f7acea0ef1a8ef
Ibrat <ibrat@abutech.uz>
2022-01-30 22:48:40 +0500

    feat: update tag

--- src/modules/api/tags/tag-api.controller.ts
+++ src/modules/api/tags/tag-api.controller.ts
@@ -1,7 +1,16 @@
-import { Post, Controller, Body } from '@nestjs/common';
+import {
+  Post,
+  Controller,
+  Body,
+  Put,
+  Param,
+  NotFoundException
+} from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
   ApiTags
 } from '@nestjs/swagger';
 
@@ -10,6 +19,8 @@ import { CreateTagDto } from '../../tags/dto/CreateTag';
 import { TagDto } from '../../tags/dto/TagDto';
 import { ValidationErrorExeption } from '../../../shared/exception/ValidationErrorExeption';
 import { Tag } from '../../../entities/Tag';
+import { UpdateResult } from 'typeorm';
+import { UpdateTagDto } from '../../tags/dto/UpdateTag';
 
 @Controller('/tags')
 @ApiTags('tags')
@@ -21,5 +32,42 @@ export class TagDashboardController {
   @ApiBadRequestResponse({ type: ValidationErrorExeption })
   async createTag(@Body() tag: CreateTagDto): Promise<Tag> {
     return this.tagService.create(tag);
+  }
+
+  @Put(':id')
+  @ApiOkResponse({ description: 'Ok' })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      type: 'String',
+      example: {
+        statusCode: 400,
+        message: ['name must be a string'],
+        error: 'Bad Request'
+      }
+    }
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found',
+    schema: {
+      type: 'String',
+      example: {
+        statusCode: 404,
+        message: 'Tag not found!',
+        error: 'Not Found'
+      }
+    }
+  })
+  async updateTag(
+    @Param('id') id: string,
+    @Body() tag: UpdateTagDto
+  ): Promise<UpdateResult> {
+    const isExistsTag = await this.tagService.getTag(id);
+
+    if (isExistsTag === undefined) {
+      throw new NotFoundException('Tag not found!');
+    }
+
+    return this.tagService.update(id, tag);
   }
 }
