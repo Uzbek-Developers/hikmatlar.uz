e37b4255d8f1fa9f246f6ed558389779666a685c
Ibrat <ibrat@abutech.uz>
2022-02-07 14:49:45 +0500

    refactor: add validation create table: if not exists

--- src/migrations/1644225684643-create_table_categories.ts
+++ src/migrations/1644225684643-create_table_categories.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class createTableCategories1644225684643 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      ` CREATE TABLE "categories" (
+      ` CREATE TABLE IF NOT EXISTS "categories" (
             "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
             "name" varchar
       );`
