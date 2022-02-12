27b9ce86aa3d9d54182a028831ef84cb35e61b19
akhmadiy <ahmadulinjs@gmail.com>
2022-02-12 23:23:13 +0500

    feat: import quote service to root

--- src/modules/root/root.module.ts
+++ src/modules/root/root.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RootController } from './root.controller';
 import { RootService } from './root.service';
 import { AuthorModule } from '../author/author.module';
+import { QuoteModule } from '../quote/quote.module';
 
 @Module({
-  imports: [AuthorModule],
+  imports: [AuthorModule, QuoteModule],
   controllers: [RootController],
   providers: [RootService]
 })
--- src/modules/root/root.service.ts
+++ src/modules/root/root.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { AuthorService } from '../author/author.service';
+import { QuoteService } from '../quote/quote.service';
 @Injectable()
 export class RootService {
-  constructor(private readonly authorService: AuthorService) {}
+  constructor(
+    private readonly authorService: AuthorService,
+    private readonly quoteService: QuoteService
+  ) {}
   async getHomePageData(): Promise<any> {
     const lastAuthors = await this.authorService.getLastAuthors();
     return {
